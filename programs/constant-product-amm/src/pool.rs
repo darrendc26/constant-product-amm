22702538d528e32858b813cf00514d7e35db7364
Darren <darren26feb@gmail.com>
2025-08-17 17:41:28 +0530

    Pool Account

--- programs/constant-product-amm/src/pool.rs
+++ programs/constant-product-amm/src/pool.rs
@@ -1,6 +1,16 @@
 use anchor_lang::prelude::*;
 
-#[account]
+    #[account]
+    #[derive(InitSpace)]
     pub struct Pool {
-  
+    pub authority: Pubkey,
+    pub token_a: Pubkey,
+    pub token_b: Pubkey,
+    pub token_a_vault: Pubkey,
+    pub token_b_vault: Pubkey,
+    pub fee_vault: Pubkey,
+    pub total_lp: u64,
+    pub fee: u64,
+    pub k: u64,
+    pub bump: u8,
 }
