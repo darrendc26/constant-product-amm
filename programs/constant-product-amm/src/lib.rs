4e76a0edad3095bb32564574f49ca9a4e273e168
Darren <darren26feb@gmail.com>
2025-08-17 22:44:07 +0530

    lib.rs fn added

--- programs/constant-product-amm/src/lib.rs
+++ programs/constant-product-amm/src/lib.rs
@@ -10,6 +10,8 @@ pub mod swap;
 pub mod errors;
 
 use initialize_pool::*;
+use add_liquidity::*;
+use swap::*;
 
 declare_id!("HoTFuDYDyVkeug3eyZ6Xx4gsjxkG3qa6NZLXqzGtYHge");
 
@@ -19,5 +21,13 @@ pub mod constant_product_amm {
 
     pub fn initialize_pool(ctx: Context<InitializePool>) -> Result<()> {
         initialize_pool_handler(ctx)
+    }
+
+    pub fn add_liquidity(ctx: Context<AddLiquidity>, amount_a: u64, amount_b: u64) -> Result<()> {
+        add_liquidity_handler(ctx, amount_a, amount_b)
+    }
+
+    pub fn swap(ctx: Context<Swap>, amount_in: u64, a_to_b : bool) -> Result<()> {
+        swap_handler(ctx, amount_in, a_to_b)
     }
 }
